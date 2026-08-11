a1cbe5e468d9563f0674adea900fcac6d6c1238b
hajji-Ismail <hajji.ismail.851@gmail.com>
2026-08-11 18:32:41 +0100

    adding docker to run the databases

--- scraping.py
+++ scraping.py
@@ -1,5 +1,13 @@
 import pymysql 
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 conn = pymysql.connect(
-    
+    host= os.getenv("HOST"),
+    user= os.getenv("USER"),
+    password= os.getenv("PASSWORD"),
+    db=os.getenv("DB"),
+    cursorclass=pymysql.cursors.DictCursor
 )
